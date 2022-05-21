9c3a9b5b996918aaeb0a72e849c8493fab3f68b3
0xSkash <skashtwitter@gmail.com>
2022-05-21 16:17:03 +0200

    added api auth token to build config fields

--- app/src/main/java/de/skash/movielist/feature/movie/MovieFragment.kt
+++ app/src/main/java/de/skash/movielist/feature/movie/MovieFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import de.skash.movielist.BuildConfig
 import de.skash.movielist.databinding.FragmentMoviesBinding
 
 class MovieFragment : Fragment() {
@@ -23,8 +24,7 @@ class MovieFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentMoviesBinding.bind(view)
-
-
+        
     }
 
     override fun onDestroyView() {
